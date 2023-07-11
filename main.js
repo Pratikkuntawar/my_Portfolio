11b1fc67c8beffa69cb427db351b136579420262
Pratikkuntawar <137525410+Pratikkuntawar@users.noreply.github.com>
2023-07-11 08:06:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const typed=new Typed(".Multiple-text",{
+    strings:["Frontend Developer","Web developer","UI/UX Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
